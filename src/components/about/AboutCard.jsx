99dedd2ddbd01f44dc4c54c23ccab98efceb8ba1
Saran3072 <saran30702@gmail.com>
2023-03-31 15:34:11 +0530

    About

---
+++ src/components/about/AboutCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AboutCard = () => {
+  return (
+    <>
+        <section className="aboutHome">
+            <div className="container flexSB">
+                <div className="left row">
+                    <img src="./images/about.jpg" alt="" />
+                </div>
+                <div className="right row">
+                    <Title subtitle= 'LEARN ANYTHING' title = 'Benefits about Pragna classes' />
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default AboutCard
